989bbc2894b56955876e4ab40dcb3cce4d04f9f6
Nardgelen Jean Francois <nardgelenjf@gmail.com>
2024-04-12 18:53:56 -0400

    file structure and user/home routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+const { User } = require("../models");
+// const withAuth = require("../utils/auth.js");
+// const axios = require('axios');
+
+// Display main page with search functionality
+router.get("/", async (req, res) => {
+  try {
+    // const bookData = await Book.findAll({
+    //   attributes: ["bookTitle", "Author"],
+    //   include: [{ model: Post, attributes: ["postTitle"] }],
+    // });
+    // const books = bookData.map((book) => book.get({ plain: true }));
+    // console.log(books);
+    res.render("homepage");
+    // Fetch posts or perform any necessary logic to display main page
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+router.get("/profile", async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("profile", {
+      // ...user,
+      // logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // if (req.session.logged_in) {
+  //   res.redirect("/profile");
+  //   return;
+  // }
+
+  res.render("login");
+});
+router.get("/signup", (req, res) => {
+  console.log("Signup");
+  // if (req.session.logged_in) {
+  //   res.redirect("/");
+  //   return;
+  // }
+
+  res.render("signup");
+});
+
+module.exports = router;
---
+++ public/login.js
@@ -0,0 +1,27 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+  // Collect values from the login form
+  //   const username = document.querySelector("#username").value.trim();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    console.log("Request body:", { email, password }); // Log the request body
+
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
